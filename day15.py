0ac3ca4c22129ed7d80c30847201663d35ab5d3c
sravyarajana19 <165186489+sravyarajana19@users.noreply.github.com>
2026-05-30 14:24:52 +0530

    Add files via upload
    Day 15/100 of #100DaysOfCode 🚀
    
    Learned Python modules and explored commonly used modules like `math`, `os`, `sys`, `random`, and `collections`. 🐍
    
    Also practiced concepts such as `append()` vs `extend()`, `is` vs `==`, mutable vs immutable data types, memory allocation, and generators to strengthen my understanding of Python fundamentals. 💻
    
    #Python #CodingJourney #Learning

---
+++ day15.py
@@ -0,0 +1,88 @@
+'''
+module:
+a modules in python is a file that contains python code such as
+-variables
+-functions
+-classesss
+-statements
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+
+-------------------------------
+import math
+print(math.sqrt(5))
+
+print(math.factorial(8))
+print(math.pow(2,5))
+
+
+
+from math import sqrt
+print(sqrt(5))
+
+-------------------------------
+import math as m.
+
+-----------------------------
+import os
+os.mkdir("code_ex")
+
+import os
+os.remove("text doc.txt")
+-------------------------------
+
+
+import sys
+print(sys.version)
+print(sys.exit)
+print(sys.path)
+-------------------------------
+
+import random
+print(random.randint(1000,9999))
+
+-------------------------------
+
+
+from collections import Counter,defaultdict
+data = ['s','r','a','v']
+counter = Counter(data)
+print(counter)
+ss = defaultdict(int)
+ss['missing'] += 1
+print(ss['missing'])
+print(ss)
+------------------------------
+'''
+
+extend , append
+is, ==
+mutable, immutable
+memory allocation in python
+generators
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
